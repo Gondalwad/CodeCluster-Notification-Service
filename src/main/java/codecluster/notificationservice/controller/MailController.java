8d4c47b9a68e35e4873cd3dba9cae0412c760c19
Shripad Dhanshetti <shripad1483@gmail.com>
2026-08-05 19:21:56 +0530

    feat(MailController.java): implement otp mail feature for user verification

---
+++ src/main/java/codecluster/notificationservice/controller/MailController.java
@@ -0,0 +1,28 @@
+package codecluster.notificationservice.controller;
+
+import codecluster.notificationservice.dto.SendMailDto;
+import codecluster.notificationservice.dto.SentMailDto;
+import codecluster.notificationservice.service.MailerService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/mail")
+public class MailController {
+
+    @Autowired
+    private MailerService mailerService;
+
+    @PostMapping("/send")
+    public ResponseEntity<SentMailDto> sendMail(
+            @Valid @RequestBody SendMailDto dto) {
+
+        SentMailDto response = mailerService.trySend(dto);
+        return ResponseEntity.ok(response);
+    }
+}
